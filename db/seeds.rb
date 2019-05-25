565d4ac6ed628b78d17492d6ec9f5aff0925c674
Luis Eugenio <luis.eugenio@hubtec.io>
2019-05-25 01:50:10 -0300

    Fake data seed Using gem Faker to provide 150 ShortUrl with Random visit_count

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require "faker"
+
+150.times do
+	short_url = ShortUrl.new(:url => Faker::Internet.unique.url, :title => Faker::Internet.unique.domain_word, :visit_count => SecureRandom.random_number(100))
+	short_url.save
+end
