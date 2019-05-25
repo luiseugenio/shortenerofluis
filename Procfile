afbc086b9f1e5d153653b0cb4066481fc32f72bb
Luis Eugenio <luis.eugenio@hubtec.io>
2019-05-25 18:13:46 -0300

    Configure sidekiq to Heroku

--- Procfile
+++ Procfile
@@ -1,2 +1,2 @@
 web: bin/rails server -p $PORT -e $RAILS_ENV
-worker: bundle exec sidekiq -q default
+worker: bundle exec sidekiq -e $RACK_ENV -C config/sidekiq.yml
--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,7 +1,20 @@
-Sidekiq.configure_server do |config|
-  config.redis = { url: 'redis://localhost:6379/0' }
+
+if Rails.env.development?
+  Sidekiq.configure_server do |config|
+	  config.redis = { url: 'redis://localhost:6379/0' }
+	end
+
+	Sidekiq.configure_client do |config|
+	  config.redis = { url: 'redis://localhost:6379/0' }
+	end
 end
 
-Sidekiq.configure_client do |config|
-  config.redis = { url: 'redis://localhost:6379/0' }
+if Rails.env.production?
+  Sidekiq.configure_client do |config|
+    config.redis = { url: ENV['REDIS_URL'], size: 2 }
+  end
+
+  Sidekiq.configure_server do |config|
+    config.redis = { url: ENV['REDIS_URL'], size: 20 }
+  end
 end
